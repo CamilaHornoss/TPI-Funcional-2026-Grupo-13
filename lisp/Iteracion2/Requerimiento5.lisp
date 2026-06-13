84da4c02af736921b592213a7cd3e672a5107d4e
VeroSGV <89225772+VeroSGV@users.noreply.github.com>
2026-06-13 06:57:08 +0000

    No se realizaron cambios

---
+++ lisp/Iteracion2/Requerimiento5.lisp
@@ -0,0 +1,6 @@
+
+;; =============================================================================
+;; REQUERIMIENTO 5: No fue necesario implementar algun cambio, ya que la duración 
+;; se actualiza con respecto a la función duracion-ciclo del requerimiento(4) anterior 
+;; =============================================================================
+
