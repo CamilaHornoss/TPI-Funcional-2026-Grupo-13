a5336b4f7e1474543a3206f5386d10337e5e7ae7
Camila Paloma Hornos <cpalomahornos@gmail.com>
2026-06-16 23:29:38 -0300

    organization

--- lisp/Iteracion2/Requerimiento5.lisp
+++
@@ -1,6 +0,0 @@
-
-;; =============================================================================
-;; REQUERIMIENTO 5: No fue necesario implementar algun cambio, ya que la duración 
-;; se actualiza con respecto a la función duracion-ciclo del requerimiento(4) anterior 
-;; =============================================================================
-
